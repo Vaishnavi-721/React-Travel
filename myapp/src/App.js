c96ee249f4848cad34d3ef8d4dfb77db00ef0b3e
Vaishnavi <vaishnavisingh0721@gmail.com>
2023-02-12 02:21:43 +0530

    changes1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router ,Routes , Route } from 'react-router-dom'
 import Home from './components/pages/Home'
+import HeroSection from './components/HeroSection';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
  
 
 function App() {
@@ -11,7 +15,11 @@ function App() {
      <Router>
          <Navbar/>
          <Routes>
-          <Route path='/' exact component={Home}/>
+          <Route index  element ={<div><Home/></div>}/>
+          <Route path='hero' element={<HeroSection/>}/>
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
          </Routes>
      </Router>
        
